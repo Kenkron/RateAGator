1ef5fbc3c804f1e31e769aa9d4b70ca650b04ae8
Christopher Waugh <kenkron@gmail.com>
2013-01-30 21:27:46 -0500

    Previos commit went wrong.  Redoing

---
+++ RateAGator/src/com/breadcrumbteam/rateagator/MainActivity.java
@@ -0,0 +1,30 @@
+package com.breadcrumbteam.rateagator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public void search(View view){
+		String text=((EditText)findViewById(R.id.searchBar)).getText().toString();
+		Log.println(Log.INFO, "Update", "Searching: "+text);
+	}
+	
+}
