aafa8b46cb4b87087d4cd49e7f43c84b25014263
Christopher Waugh <kenkron@gmail.com>
2013-02-17 15:53:17 -0500

    Adding proffessor page that lists all courses under that professor.  Note that, at the time of this commit, all of the courses will be listed for a professor, taking a very long time to load the page.

--- RateAGator/src/com/breadcrumbteam/rateagator/ProfessorPage.java
+++ RateAGator/src/com/breadcrumbteam/rateagator/ProfessorPage.java
@@ -1,16 +1,54 @@
 package com.breadcrumbteam.rateagator;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 
 public class ProfessorPage extends Activity {
 
+	//the labels for the intent fields
+	public static final String INTENT_FIRST_NAME="first name";
+	public static final String INTENT_LAST_NAME="last name";
+	
+	Professor currentProfessor;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		//TODO make a layout for ProfessorPage
 		setContentView(R.layout.professor_page);
+		
+		try{
+		currentProfessor=DBConnector.getProfessor(
+				getIntent().getStringExtra(INTENT_FIRST_NAME), 
+				getIntent().getStringExtra(INTENT_LAST_NAME));
+		}catch (InterruptedException E){
+			//I have no Idea how to handle an interrupted exception
+		}
+		
+		ViewGroup resultsList=(ViewGroup)findViewById(R.id.professorEvalList);
+		
+		for (Course c:currentProfessor.courseList){
+			Log.d("ProfessorPage", "CourseList: "+c.courseName);
+			Button currentResult=new Button(this);
+			currentResult.setText(c.courseName);
+			currentResult.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+			currentResult.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					String text=((Button)v).getText().toString();
+					Log.d("SearchResults","clicked on "+text);
+				}
+			});
+			resultsList.addView(currentResult);
+		}
 	}
 	
 	//This method is called when button is pressed
