31e7942e55d1bc64b47fe7792d85f4f886fc8592
wwiatt <wwiatt@ufl.edu>
2013-03-20 09:08:47 -0400

    Added Ratings Class

---
+++ RateAGator/src/com/breadcrumbteam/rateagator/Rating.java
@@ -0,0 +1,55 @@
+package com.breadcrumbteam.rateagator;
+
+/**
+ * 
+ * @author westwiatt
+ *
+ *Types of Ratings:
+ *Responded
+ *OverallRating
+ *Language
+ *Work
+ *Difficulty
+ *Book
+ *Hotness
+ *Clarity Helpfulness
+ *Course Relevance
+ *
+ */
+
+public class Rating {
+	public static final String[] FIELD_NAMES=
+		{
+			"OverallRating ",
+			"Language Proficiency ",
+			"Work Load ",
+			"Difficulty ",
+			"Book ",
+			"Hotness ",
+			"Clarity Helpfulness ",
+			"Course Relevance "
+		};
+	
+	private int totalResponses;
+	private double[] responses = new double[8];
+	private int currentResponse = 0;
+	
+	public Rating(int numOfResponses) {
+		this.totalResponses = numOfResponses;
+	}
+	
+	public void addResponseValue(double responseValue) {
+		if(currentResponse < 8) {
+			responses[currentResponse] = responseValue;
+			currentResponse++;
+		}
+	}
+	
+	public int getTotalRatingResponses() {
+		return totalResponses;
+	}
+	
+	public double[] getRatingResponses() {
+		return responses;
+	}
+}
