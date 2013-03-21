af6d480b5c708941eab66a9b0a27d3bc3b88fa5d
Kyle Koceski <koceskik@gmail.com>
2013-03-21 16:54:32 -0400

    add ArrayList textbooks

--- RateAGator/src/com/breadcrumbteam/rateagator/Course.java
+++ RateAGator/src/com/breadcrumbteam/rateagator/Course.java
@@ -1,20 +1,25 @@
 package com.breadcrumbteam.rateagator;
 
+import java.util.ArrayList;
+
 /**this is an object representing a particular course under
  * a particular professor.  It contains the name of the course,
  * the number of the course, the name of the professor, and 
  * a list of the evaluations for this professor-specific course*/
 public class Course {
 	
-	/**The human readable name of the course*/
+	/*The human readable name of the course*/
 	protected String courseName;
 	
-	/**The cryptic number assigned to this course
+	/*The cryptic number assigned to this course
 	 * usually three letters followed by four numbers*/
 	protected String courseNum;
 	
 	/*The professor teaching this particular course*/
 	protected Professor courseProf;
+	
+	/*ArrayList of strings of textbook URLs or ISBN*/
+	protected ArrayList<String> textbook;
 	
 	public Course(String courseName, String courseNum){
 		this.courseName = courseName;
@@ -26,5 +31,12 @@ public class Course {
 	 */
 	public void setCourseProf(Professor p){
 		courseProf = p;
+	}
+	
+	public void setTextbook(ArrayList<String> t) {
+		this.textbook = t;
+	}
+	public static boolean isURL(String txtbk) {
+		return txtbk.startsWith("http");
 	}
 }
