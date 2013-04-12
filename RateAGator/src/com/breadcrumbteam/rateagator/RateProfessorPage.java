ab7b595eec4c55dffa27325088d856820cacf786
Kyle Koceski <koceskik@gmail.com>
2013-04-12 17:20:18 -0400

    Submission button

--- RateAGator/src/com/breadcrumbteam/rateagator/RateProfessorPage.java
+++ RateAGator/src/com/breadcrumbteam/rateagator/RateProfessorPage.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -17,6 +19,10 @@ public class RateProfessorPage extends Activity {
 	public static final String INTENT_PROFESSOR_FIRST_NAME = "first name";
 	public static final String INTENT_PROFESSOR_LAST_NAME = "last name";
 	public static final String INTENT_COURSE_NUMBER = "course number";
+	private String fName = null;
+	private String lName = null;
+	private String cCode = null;
+	private RatingBar[] ratingBar = null;
 
 	/** displays the rating currently on display */
 
@@ -25,33 +31,44 @@ public class RateProfessorPage extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.rate_professor_page);
 
-		Log.d("RatingsPage",
-				"name (first): "
-						+ getIntent().getStringExtra(
-								INTENT_PROFESSOR_FIRST_NAME));
-		Log.d("RatingsPage",
-				"name (last): "
-						+ getIntent()
-						.getStringExtra(INTENT_PROFESSOR_LAST_NAME));
-		Log.d("RatingsPage",
-				"course: " + getIntent().getStringExtra(INTENT_COURSE_NUMBER));
+		fName = getIntent().getStringExtra(INTENT_PROFESSOR_FIRST_NAME);
+		lName = getIntent().getStringExtra(INTENT_PROFESSOR_LAST_NAME);
+		cCode = getIntent().getStringExtra(INTENT_COURSE_NUMBER);
 		
+		ViewGroup container = (ViewGroup) (findViewById(R.id.rateProfessorFieldList));
+		ratingBar = new RatingBar[Rating.FIELD_NAMES.length];
 		for(int i = 0;i<Rating.FIELD_NAMES.length;i++) {
-			ViewGroup container = (ViewGroup) (findViewById(R.id.rateProfessorFieldList));
-			LinearLayout fullRate = new LinearLayout(this);
-			fullRate.setOrientation(LinearLayout.HORIZONTAL);
 			TextView newRatingLabel = new TextView(this);
 			newRatingLabel.setText(Rating.FIELD_NAMES[i]);
 			
-			RatingBar ratingBar = new RatingBar(this, null, android.R.attr.ratingBarStyle);
-			ratingBar.setNumStars(5);
-			ratingBar.setRating(3);
-			ratingBar.setStepSize(1);
+			LinearLayout fullRate = new LinearLayout(this);
+			fullRate.setOrientation(LinearLayout.HORIZONTAL);
+			
+			ratingBar[i] = new RatingBar(this, null, android.R.attr.ratingBarStyle);
+			ratingBar[i].setNumStars(5);
+			ratingBar[i].setRating(3);
+			ratingBar[i].setStepSize(1);
 			
 			fullRate.addView(newRatingLabel);
-			fullRate.addView(ratingBar);
+			fullRate.addView(ratingBar[i]);
 			container.addView(fullRate);
 		}
+		LinearLayout fullRate = new LinearLayout(this);
+		fullRate.setOrientation(LinearLayout.HORIZONTAL);
+		
+		Button submit = new Button(this);
+		submit.setText("Submit Rating");
+		submit.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Rating r = new Rating(Rating.FIELD_NAMES.length);
+				for(int i = 0;i<Rating.FIELD_NAMES.length;i++) {
+					r.addResponseValue(ratingBar[i].getNumStars());					
+				}
+				DBConnector.addRatings(fName,lName,cCode, r);
+			}
+		});
+		fullRate.addView(submit);
+		container.addView(fullRate);
 	}
 
 	public void goToComments(View view) {
