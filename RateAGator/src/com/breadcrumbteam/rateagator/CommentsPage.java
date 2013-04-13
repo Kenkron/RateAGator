f6b9ff2d45f3f43f99fa35a3f44a0dc2a851856e
Alex King <alexhughesking@gmail.com>
2013-04-13 18:24:39 -0400

    Sign in to comment. Comment backgrounds. Trim.

--- RateAGator/src/com/breadcrumbteam/rateagator/CommentsPage.java
+++ RateAGator/src/com/breadcrumbteam/rateagator/CommentsPage.java
@@ -2,11 +2,13 @@ package com.breadcrumbteam.rateagator;
 
 import java.util.ArrayList;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -15,13 +17,15 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.support.v4.app.NavUtils;
+import android.view.View.OnClickListener;
+import android.content.DialogInterface;
 
 public class CommentsPage extends Activity {
 	
 
 	public ArrayList<String> comments;
 	TextView noComments = null;
+	boolean bgFlag;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +38,7 @@ public class CommentsPage extends Activity {
 		//TODO: check DBConnector.hasErrorOccurred()
 
 
-ViewGroup commentsList = (ViewGroup)findViewById(R.id.commentsList);
+		ViewGroup commentsList = (ViewGroup)findViewById(R.id.commentsList);
 		
 		if (comments == null) {
 			noComments = new TextView(this);
@@ -43,21 +47,48 @@ ViewGroup commentsList = (ViewGroup)findViewById(R.id.commentsList);
 			commentsList.addView(noComments);
 		}
 		else {
+		bgFlag = true;
 		for (int i = 0; i < comments.size(); i++) {
+			String[] uc = comments.get(i).split(":");
 			TextView newComment = new TextView(this);
-			newComment.setText(comments.get(i));
+			newComment.setText(uc[1]);
 			newComment.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+			if (bgFlag){
+				newComment.setBackgroundColor(Color.GRAY);
+			}
+			bgFlag = !bgFlag;
+			newComment.setTypeface(Typeface.DEFAULT_BOLD);
+			newComment.setTextSize(20);
 			commentsList.addView(newComment);
 		}
 		}
 
 	}
+	
 
 
 	public void addComment(View view){
 		ViewGroup commentsList = (ViewGroup)findViewById(R.id.commentsList);
 		EditText box = (EditText)findViewById(R.id.commentBox);
 		String comment = box.getText().toString();
+		InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		
+		if (MainActivity.getUsername() == null) {
+			mgr.hideSoftInputFromWindow(box.getWindowToken(), 0);
+			Toast error = Toast.makeText(this, "You must sign in to comment", Toast.LENGTH_LONG);
+			error.show();
+			return;
+		}
+		
+		comment = comment.trim();
+		if (comment.equals("")) {
+			box.setText("");
+			mgr.hideSoftInputFromWindow(box.getWindowToken(), 0);
+			Toast error = Toast.makeText(this, "You must first enter a comment", Toast.LENGTH_LONG);
+			error.show();
+			return;
+		}
 		
 		if (noComments != null) {
 			commentsList.removeView(noComments);
@@ -66,10 +97,15 @@ ViewGroup commentsList = (ViewGroup)findViewById(R.id.commentsList);
 		TextView addedComment = new TextView(this);
 		addedComment.setText(comment);
 		addedComment.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		addedComment.setTypeface(Typeface.DEFAULT_BOLD);
+		addedComment.setTextSize(20);
+		if (bgFlag) {
+			addedComment.setBackgroundColor(Color.GRAY);
+		}
 		commentsList.addView(addedComment);
 		
 		
-		InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		
 		mgr.hideSoftInputFromWindow(box.getWindowToken(), 0);
 
 		DBConnector.addComment(getIntent().getStringExtra("fName"), getIntent().getStringExtra("lName"),
